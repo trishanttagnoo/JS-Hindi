e456cef72d167d381007be338e56d86b622442aa
trishanttagnoo <36534379+trishanttagnoo@users.noreply.github.com>
2023-12-11 07:24:19 +0000

    converstion of datatypes

---
+++ 01_basics/converstionOperation.js
@@ -0,0 +1,15 @@
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+// false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "trishant" => true
